829277ee024e38a49170c5798eb94a93c84145e5
dk9394 <k.deepak9394@gmail.com>
2025-07-02 06:14:24 +0530

    home page API

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HomeService } from '../../services/home.service';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,11 +10,13 @@ describe('HomeComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [HomeComponent],
+			imports: [HomeComponent, HttpClientTestingModule],
+			providers: [HomeService],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(HomeComponent);
 		component = fixture.componentInstance;
+		TestBed.inject(HomeService);
 		fixture.detectChanges();
 	});
 
