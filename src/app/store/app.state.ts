978578f91bf313126336a9a24b130837563cbce0
dk9394 <k.deepak9394@gmail.com>
2025-07-06 23:07:37 +0530

    authentication added, handled appointment scanario

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,5 +1,7 @@
 import { DoctorsState } from './doctors/doctors.reducers';
+import { UserState } from './user/user.reducers';
 
 export interface AppState {
 	doctors: DoctorsState;
+	user: UserState;
 }
